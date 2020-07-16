15ed9958f69a55242ee526e8b87ac68eda3d17b2
davidmpye <davidmpye@gmail.com>
2020-07-16 21:59:10 +0100

    Changes to make it upload and lock

--- code/upload.sh
+++ code/upload.sh
@@ -1,7 +1,6 @@
 make && arm-none-eabi-objcopy -O binary main.elf main.bin && st-flash --reset write main.bin 0x8000000 
 [ $? -ne 0 ]  && echo "Failed to program - trying to unlock" && openocd -d0 -f stlink-v2.cfg -f stm32f1x.cfg -f ocd-unlock.cfg && echo "Trying to reprogram" &&  st-flash --reset write main.bin 0x8000000
-[ $? -ne 0 ] && echo "Still failed to program - aborting" && exit 1
-echo "Program successful - Locking"
+echo "LOCKING"
 openocd -d0 -f stlink-v2.cfg -f stm32f1x.cfg -f ocd-lock.cfg
 
 [ $? -eq 0 ] && echo "Lock returned successfully"
